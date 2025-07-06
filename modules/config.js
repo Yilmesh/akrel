e24b0dec4437067021f68b205843061ca8be4b39
Yilmesh <jordan.dylan.debeer@gmail.com>
2025-07-06 12:49:20 +0200

    base en voie de debug

--- modules/config.js
+++ modules/config.js
@@ -1,16 +1,6 @@
-export const NETHER = {};
+export const AKREL = {};
 
-NETHER.attributes = {
+AKREL.attributes = {
     
     Test: "TEST!"
-}
-
-NETHER.compLvlMultiplier = {
-
-    lvlComp1: 1,
-    lvlComp2: 0.9,
-    lvlComp3: 0.8,
-    lvlComp4: 0.6,
-    lvlComp5: 0.4,
-    lvlComp6: 0.3
 }
