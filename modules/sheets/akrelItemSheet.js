e812751714c213599fcd8f8cb09ed318773f24fb
Yilmesh <jordan.dylan.debeer@gmail.com>
2025-07-24 13:59:23 +0200

    item debug

--- modules/sheets/akrelItemSheet.js
+++ modules/sheets/akrelItemSheet.js
@@ -1,38 +1,29 @@
-// systems/akrel/module/sheets/akrelItemSheet.js
+import { AKREL } from "../config.js"; 
 
 export default class akrelItemSheet extends ItemSheet {
 
     /** @inheritdoc */
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
-            classes: ["akrel", "sheet", "item"], // Classe CSS pour ta feuille
-            // IMPORTANT : NE PAS DÉFINIR 'template' ICI si tu utilises 'get template()'
-            // Foundry utilisera la méthode 'get template()' pour déterminer le bon fichier.
+            classes: ["akrel", "sheet", "item"],
             width: 620,
             height: 500,
             tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }],
-            scrollY: [".attributes"], // Si tu as des éléments défilants
+            scrollY: [".attributes"],
         });
     }
 
     /** @inheritdoc */
-    // Cette méthode est cruciale pour choisir le bon template en fonction du type d'objet.
     get template() {
-        // Le chemin de base vers tes templates de fiches d'objets
-        const path = "systems/akrel/templates/sheets/items/"; // Assure-toi que c'est le bon dossier !
-
-        // Récupère le type de l'objet (par exemple, "weapon", "armor", "spell", "loot")
-        // et construis le chemin complet vers le fichier .hbs correspondant.
-        // Par exemple, si item.type est "weapon", ça deviendra "systems/akrel/templates/item/weapon-sheet.hbs"
+        const path = "systems/akrel/templates/sheets/items/";
         return `${path}${this.item.type}-sheet.hbs`;
     }
 
     /** @inheritdoc */
     getData() {
         const data = super.getData();
-
-        // Ajoute ici tes données de configuration si nécessaire
-        data.config = CONFIG.AKREL;
+        data.config = CONFIG.AKREL; 
+        data.system = data.item.system;
 
         return data;
     }
@@ -41,7 +32,12 @@ export default class akrelItemSheet extends ItemSheet {
     activateListeners(html) {
         super.activateListeners(html);
 
-        // Tu peux ajouter d'autres écouteurs d'événements ici si besoin.
         if ( !this.isEditable ) return;
+
+        // Ajoutez ici vos écouteurs d'événements (boutons, etc.)
+        // Exemple pour le bouton de dé (si vous en avez un)
+        // html.find('.roll-damage').click(this._onRollDamage.bind(this));
     }
+
+    // Définissez ici vos méthodes d'écouteurs, ex: _onRollDamage(event) { ... }
 }
